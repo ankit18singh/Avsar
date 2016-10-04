4123b70697984d8b623b4c99e53387b492627a97
ankit18singh <aj.ankitsingh@gmail.com>
2016-10-04 21:37:26 +0530

    UI fixed for all browser.:

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
                 $(document).scroll(function() { 
                     scroll_start = $(this).scrollTop();
                     if(scroll_start > offset.top) {
-                          $(".navbar-default").css('background-image', 'url(/img/Banner-Background-007.jpg  )');
+                          $(".navbar-default").css('background-image', "url(img/Banner-Background-007.jpg)");
                           $(".navbar-brand").css('display','block');
                        } else {
                           $('.navbar-default').css('background-color', 'transparent');
